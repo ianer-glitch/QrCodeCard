7f0dfd8c75e60e9287d459c7ce7d92c65fd6c10b
ianer-glitch <ianrepker@gmail.com>
2025-01-16 18:16:19 -0300

    css changes in qr-code

--- src/app/views/qr-code/qr-code.component.ts
+++ src/app/views/qr-code/qr-code.component.ts
@@ -16,8 +16,23 @@ export class QrCodeComponent {
   }
 
   imgUrl = signal('')
-
+  rotX = signal(0)
+  rotY = signal(0)
   ngOnInit(){
+    this.getQrCode()
+    this.handle3d()
+  }
+  handle3d(){
+    window.addEventListener("mousemove",e=>{
+      const w  = window.innerWidth;
+      const h = window.innerWidth;
+
+      this.rotX.set(7*2-e.pageY *(14*2/h))
+      this.rotY.set(8*2-e.pageX*(16*2/w))
+    })
+  }
+
+  getQrCode(){
     const data = this.local.getLocalStorage("qrCode")
     if(data){
       this.api.getQrCode("200",data).subscribe((response)=>

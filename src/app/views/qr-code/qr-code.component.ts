4d32b2d5a62e3efc68b8a0e587f2f439e9be6141
ianer-glitch <ianrepker@gmail.com>
2025-01-18 15:25:56 -0300

    added device orientation handler in qr-code

--- src/app/views/qr-code/qr-code.component.ts
+++ src/app/views/qr-code/qr-code.component.ts
@@ -18,24 +18,64 @@ export class QrCodeComponent {
   imgUrl = signal('')
   rotX = signal(0)
   rotY = signal(0)
+  text = signal('')
   ngOnInit(){
     this.getQrCode()
     this.handle3d()
   }
   handle3d(){
+    this.handleMouseMove()
+    // this.handleDeviceMove()
+    this.handleDeviceMove()
+  }
+  
+  handleMouseMove(){
     window.addEventListener("mousemove",e=>{
       const w  = window.innerWidth;
       const h = window.innerWidth;
-
-      this.rotX.set(7*2-e.pageY *(14*2/h))
-      this.rotY.set(8*2-e.pageX*(16*2/w))
+      
+      this.rotX.set(7*4-e.pageY *(14*4/h))
+      this.rotY.set(8*4-e.pageX*(16*4/w))
     })
+    
   }
+  
+  shouldRotate = signal(false)
+  
+  handleDeviceMove(){
+    window.addEventListener("deviceorientation",e=>{
+
+
+      const limitValue = 40
+      const tempX = Math.round(e.beta ?? 0) -40
+      
+      
+      const x = Math.max(-limitValue,Math.min(tempX,limitValue))
+      
+      // Todo: Add treshold for should rotate indicator
+      this.shouldRotate.set(x === -limitValue || tempX === limitValue)
+      
+      
+      
+      const y = Math.round(e.gamma ?? 0)
+      
+      
+      this.rotX.set(x)
+      this.rotY.set(y)
+
+      // this.rotX.set(7*16-x *(14*16/h))
+      // this.rotY.set(8*8-y*(16*8/w))
+    
+    })
+    
+  }
+
+ 
 
   getQrCode(){
     const data = this.local.getLocalStorage("qrCode")
     if(data){
-      this.api.getQrCode("200",data).subscribe((response)=>
+      this.api.getQrCode("150",data).subscribe((response)=>
         this.imgUrl.set(URL.createObjectURL(response))
       )
     }
